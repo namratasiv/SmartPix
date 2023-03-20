2416e2aefae7db06921f5917b6529beb688a1e4f
Sivakumar,Namrata <namrata_sivakumar@comcast.com>
2023-03-20 12:05:30 +0530

    Image Upload

---
+++ SmartPix/FormView.swift
@@ -0,0 +1,27 @@
+//
+//  FormView.swift
+//  SmartPix
+//
+//  Created by Sivakumar, Namrata on 16/03/23.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    var body: some View {
+        NavigationView{
+            Form{
+                Section {
+                    
+                }
+            }
+            .navigationTitle("Upload a pic")
+        }
+    }
+}
+
+struct FormView_Previews: PreviewProvider {
+    static var previews: some View {
+        FormView()
+    }
+}
